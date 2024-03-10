25f04e546d65819794574b97090392d844773ce9
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2024-03-10 19:10:56 -0300

    added toString() method

--- src/main/kotlin/components/Position.kt
+++ src/main/kotlin/components/Position.kt
@@ -6,5 +6,8 @@ data class Position (val startOffset: Int = 0,
                      val endLine: Int = 0,
                      val startColumn: Int = 0,
                      val endColumn: Int = 0) {
-
+    override fun toString(): String {
+        return "{\n\tstartOffset: $startOffset,\n\tendOffset: $endOffset,\n\tstartLine: $startLine," +
+                "\n\tendLine: $endLine,\n\tstartColumn: $startColumn,\n\tendColumn: $endColumn}"
+    }
 }
