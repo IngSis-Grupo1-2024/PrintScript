c80f786cc63b75168136940401559eb8d25a0b97
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2024-03-11 18:10:17 -0300

    add some logic to the reset of the token types in the ComparatorTokenType

--- src/main/kotlin/components/ComparatorTokenType.kt
+++ src/main/kotlin/components/ComparatorTokenType.kt
@@ -1,21 +1,32 @@
 package components
 
 class ComparatorTokenType : Comparator<TokenType> {
+    private val leafTypes = listOf(TokenType.TYPE, TokenType.VALUE, TokenType.IDENTIFIER)
+
     // it returns 0 if they are equals
         // for example in ASSIGNATION, you have to compare the values
     // it returns 1 if o1 > o2
     // it returns -1 if o1 < o2
     override fun compare(o1: TokenType?, o2: TokenType?): Int {
-        if(o1 == o2) return 0
-        else if (o1 == TokenType.SEMICOLON) return 1
-        else if (o1 == TokenType.LET_KEYWORD) return checkSemicolon(o2)
-        else if (o1 == TokenType.ASSIGNATION) return checkSemicolonAndLet(o2)
-        return -1
+        return when (o1) {
+            o2 -> 0
+            TokenType.SEMICOLON -> 1
+            TokenType.KEYWORD -> checkSemicolon(o2)
+            TokenType.ASSIGNATION -> checkSemicolonAndLet(o2)
+            TokenType.TYPE -> checkLeaf(o2)
+            TokenType.VALUE -> checkLeaf(o2)
+            TokenType.IDENTIFIER -> checkLeaf(o2)
+            else -> -1
+        }
+    }
+
+    private fun checkLeaf(o2: TokenType?): Int {
+        return if(o2 in leafTypes) 0 else -1
     }
 
     private fun checkSemicolonAndLet(o2: TokenType?): Int {
         if(checkSemicolon(o2) == -1) return -1
-        return if (o2 == TokenType.LET_KEYWORD) -1 else 1
+        return if (o2 == TokenType.KEYWORD) -1 else 1
     }
 
     private fun checkSemicolon(o2: TokenType?) : Int {
--- src/test/kotlin/modules/parser/ParserDeclaration.kt
+++ src/test/kotlin/modules/parser/ParserDeclaration.kt
@@ -10,19 +10,19 @@ class ParserDeclaration {
         val parser = Parser()
         val position = Position()
         val tokens: List<Token> = listOf(
-            Token(position, "", TokenType.LET_KEYWORD),
+            Token(position, "let", TokenType.KEYWORD),
             Token(position, "x", TokenType.IDENTIFIER),
             Token(position, ":", TokenType.ASSIGNATION),
-            Token(position, "", TokenType.STRING_TYPE),
+            Token(position, "string", TokenType.TYPE),
             Token(position, "", TokenType.SEMICOLON),
         )
         val astExpected: ASTInterface =
             AST(
                 Token(position, "", TokenType.SEMICOLON),
-                    AST(Token(position, "", TokenType.LET_KEYWORD),
+                    AST(Token(position, "let", TokenType.KEYWORD),
                         AST(Token(position, ":", TokenType.ASSIGNATION),
                             AST(Token(position, "x", TokenType.IDENTIFIER), null, null),
-                            AST(Token(position, "", TokenType.STRING_TYPE), null, null)),
+                            AST(Token(position, "string", TokenType.TYPE), null, null)),
                     null),
                 null)
         println(parser.parse(tokens))
