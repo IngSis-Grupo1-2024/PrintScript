411c9a64932e141993f91697658d161015eaec19
franciscocavallaro <ccavallaro00@gmail.com>
2024-03-12 18:52:44 -0300

    Lexer tests and lexer main running. Space before the semicolon is not being taken as a value now

--- src/main/kotlin/modules/lexer/Main.kt
+++ src/main/kotlin/modules/lexer/Main.kt
@@ -8,17 +8,19 @@ fun main(args: Array<String>) {
     val array1: List<Token> = lexer1.tokenize("let a: string = \"hola\";")
     for (i in array1) {
         println(i.type)
-        println()
-    }
-
-    val lexer2 = Lexer(Position(1, 1, 1, 1,1,1))
-    val array2: List<Token> = lexer2.tokenize("let a: number = 3;")
-    for (i in array2) {
-        println(i.type)
         println(i.value)
         println(i.position)
         println()
     }
+
+//    val lexer2 = Lexer(Position(1, 1, 1, 1,1,1))
+//    val array2: List<Token> = lexer2.tokenize("let a: number = 3;")
+//    for (i in array2) {
+//        println(i.type)
+//        println(i.value)
+//        println(i.position)
+//        println()
+//    }
 
 
 }
