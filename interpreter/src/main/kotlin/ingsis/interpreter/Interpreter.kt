867625b517e960deb9563972ea6bde34ff338875
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2024-04-12 17:07:05 -0300

    add interpreter by version

--- interpreter/src/main/kotlin/ingsis/interpreter/Interpreter.kt
+++ interpreter/src/main/kotlin/ingsis/interpreter/Interpreter.kt
@@ -1,10 +1,25 @@
 package ingsis.interpreter
 
 import components.statement.Statement
-import ingsis.interpreter.interpretStatement.StatementInterpreter
+import ingsis.interpreter.interpretStatement.*
 import ingsis.utils.Result
 
+object PrintScriptInterpreter {
+
+    fun createInterpreter(version: String): Interpreter {
+        return when(version) {
+            "VERSION_1" -> Interpreter(listOf(DeclarationInterpreter(), AssignationInterpreter(), CompoundAssignationInterpreter(), PrintLineInterpreter()))
+            else -> Interpreter(listOf(DeclarationInterpreter(), AssignationInterpreter(), CompoundAssignationInterpreter(), PrintLineInterpreter()))
+        }
+    }
+}
+
 class Interpreter(private val interpreters: List<StatementInterpreter>) {
+
+//    fun interpret(statement: Statement): Map<String, Result> =
+//        interpret(statement, emptyMap())
+
+
     fun interpret(
         statement: Statement,
         variableMap: HashMap<String, Result>,
