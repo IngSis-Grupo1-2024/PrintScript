dbbe87ccd3165e881dd4a12705dd1757ca850846
constanzalasarte <constanza.lasarte@ing.austral.edu.ar>
2024-04-14 09:49:46 -0300

    move rules to ingsis.formatter

--- formatter/src/main/kotlin/ingsis/formatter/Formatter.kt
+++ formatter/src/main/kotlin/ingsis/formatter/Formatter.kt
@@ -6,15 +6,15 @@ import ingsis.formatter.utils.readJsonAndStackMap
 object PrintScriptFormatter {
     fun createFormatter(version: String): Formatter {
         return when (version) {
-            "VERSION_1" -> Formatter(listOf(ScanDeclaration(), ScanAssignation(), ScanFunction(), ScanCompoundAssignation()))
-            else -> Formatter(listOf(ScanDeclaration(), ScanAssignation(), ScanFunction(), ScanCompoundAssignation()))
+            "VERSION_1" -> Formatter(listOf(ScanDeclaration(), ScanAssignation(), ScanPrintLine(), ScanCompoundAssignation()))
+            else -> Formatter(listOf(ScanDeclaration(), ScanAssignation(), ScanPrintLine(), ScanCompoundAssignation()))
         }
     }
 }
 
 class Formatter(private val scanners: List<ScanStatement>) {
     fun format(statement: Statement): String {
-        val ruleMap = readJsonAndStackMap("src/main/rules/rules.json")
+        val ruleMap = readJsonAndStackMap("formatter/src/main/kotlin/ingsis/formatter/rules/rules.json")
         val result = StringBuilder()
         for (scanner in scanners) {
             if (scanner.canHandle(statement)) {
