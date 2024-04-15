13154d0abe5c83c3485fe115eabc267280f46214
valentinavalenzi <92260481+valentinavalenzi@users.noreply.github.com>
2024-04-14 22:10:14 -0300

    changes for uploading v1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,30 +21,3 @@ tasks.register("checkHooks") {
     }
 }
 
-
-plugins {
-    java
-    `maven-publish`
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/maxigeist/PrintScript")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_AUTHOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("maven") {
-            groupId = "org.gradle.PrintScript"
-            artifactId = "library"
-            version = "1.1.0-SNAPSHOT"
-            from(components["java"])
-        }
-
-    }
-}
--- buildSrc/src/main/kotlin/PrintScript.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/PrintScript.kotlin-common-conventions.gradle.kts
@@ -78,3 +78,30 @@ tasks.register<JavaExec>("ktlintFormat") {
         "!**/build/**",
     )
 }
+
+plugins {
+    java
+    `maven-publish`
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/maxigeist/PrintScript")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_AUTHOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("maven") {
+            groupId = "org.gradle.PrintScript"
+            artifactId = "library"
+            version = "1.0.0"
+            from(components["java"])
+        }
+
+    }
+}
