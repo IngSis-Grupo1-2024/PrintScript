06e7136058f34da35d8604203e2e7db42c999998
franciscocavallaro <ccavallaro00@gmail.com>
2024-04-21 23:42:13 -0300

    Argument added to ReadInput

--- components/src/main/kotlin/ingsis/components/statement/AssignationReadInput.kt
+++ components/src/main/kotlin/ingsis/components/statement/AssignationReadInput.kt
@@ -2,10 +2,16 @@ package ingsis.components.statement
 
 import ingsis.components.Position
 
-class AssignationReadInput(private val position: Position, private val variable: Variable) : Statement {
+class AssignationReadInput(
+    private val position: Position,
+    private val variable: Variable,
+    private val argument: Value,
+) : Statement {
     override fun getStatementType(): StatementType = StatementType.ASSIGNATION_READ_INPUT
 
     fun getVariable(): Variable = variable
 
     fun getPosition(): Position = position
+
+    fun getArgument(): Value = argument
 }
--- components/src/main/kotlin/ingsis/components/statement/CompoundAssignationReadInput.kt
+++ components/src/main/kotlin/ingsis/components/statement/CompoundAssignationReadInput.kt
@@ -2,10 +2,16 @@ package ingsis.components.statement
 
 import ingsis.components.Position
 
-class CompoundAssignationReadInput(private val position: Position, private val declaration: Declaration) : Statement {
+class CompoundAssignationReadInput(
+    private val position: Position,
+    private val declaration: Declaration,
+    private val argument: Value,
+) : Statement {
     override fun getStatementType(): StatementType = StatementType.COMPOUND_ASSIGNATION_READ_INPUT
 
     fun getDeclaration(): Declaration = declaration
 
     fun getPosition(): Position = position
+
+    fun getArgument(): Value = argument
 }
