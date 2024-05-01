498e219608f25c106b61d5d03e25eca3e6744953
maxigeist <geist.maximo@gmail.com>
2024-05-01 14:57:38 -0300

    fix: add condition to if and formatting of else

--- formatter/src/main/kotlin/ingsis/formatter/scan/ScanIf.kt
+++ formatter/src/main/kotlin/ingsis/formatter/scan/ScanIf.kt
@@ -5,42 +5,77 @@ import ingsis.components.statement.Statement
 import ingsis.components.statement.StatementType
 import ingsis.formatter.Formatter
 import ingsis.formatter.PrintScriptFormatter
+import ingsis.formatter.extractor.ValueExtractor
 import ingsis.formatter.tabsCounter.IfTabs
 import ingsis.formatter.utils.FormatterRule
 
 class ScanIf : ScanStatement {
     override fun canHandle(statement: Statement): Boolean {
-        return statement.getStatementType() == StatementType.IF
+        return statement.getStatementType() == StatementType.IF || statement.getStatementType() == StatementType.ELSE
     }
 
     override fun format(
         statement: Statement,
         ruleMap: Map<String, FormatterRule>,
     ): String {
-        statement as If
         val tabs = IfTabs().getTabs(ruleMap)
-        return buildIfString(statement.getIfBlock(), tabs, ruleMap)
+        when (statement) {
+            is If -> {
+                return buildBlockWithCondition(statement, tabs, ruleMap)
+            }
+        }
+        return ""
     }
 
-    private fun buildIfString(
-        ifBlock: List<Statement>,
+    private fun buildBlockWithCondition(
+        ifStatement: If,
         tabs: String,
         ruleMap: Map<String, FormatterRule>,
     ): String {
+        val value = ValueExtractor(ifStatement.getComparison()).getValue()
         val formatter = PrintScriptFormatter.createFormatter("VERSION_2")
-        var string = "if{\n"
+        var string = "if(${value}){\n"
+        string += formatBlock(ifStatement.getIfBlock(), ruleMap, tabs, formatter)
+        string += "}"
+        if (ifStatement.getElseStatement().isNotEmpty()){
+            string += buildBlockWithoutCondition(ifStatement.getElseStatement(), tabs, ruleMap, "else")
+        }
+        return string
+
+    }
+
+    private fun buildBlockWithoutCondition(
+        ifBlock: List<Statement>,
+        tabs: String,
+        ruleMap: Map<String, FormatterRule>,
+        type: String
+    ): String {
+        val formatter = PrintScriptFormatter.createFormatter("VERSION_2")
+        var string = " ${type}{\n"
+        string += formatBlock(ifBlock, ruleMap, tabs, formatter)
+        string += "}"
+        return string
+
+    }
+
+    private fun formatBlock(
+        ifBlock: List<Statement>,
+        ruleMap: Map<String, FormatterRule>,
+        tabs: String,
+        formatter: Formatter
+    ): String {
+        var string1 = ""
         for (statement in ifBlock) {
             // We have to check if it is a print line because the print line functions in the formatting have '\n' in the
             // start so I have to put the tabs after the '\n'
-            string +=
+            string1 +=
                 if (statement.getStatementType() == StatementType.PRINT_LINE) {
                     addTabsToPrintLine(statement, ruleMap, tabs, formatter)
                 } else {
                     tabs + formatter.format(statement, ruleMap)
                 }
         }
-        string += "}"
-        return string
+        return string1
     }
 
     private fun addTabsToPrintLine(
